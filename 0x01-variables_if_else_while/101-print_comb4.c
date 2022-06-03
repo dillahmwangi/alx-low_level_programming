77db0da48d70b7deeb18db72f3330ba2e2ffa6d4
dillahmwangi <mwangimuthony@gmail.com>
2022-06-04 01:41:41 +0300

    diff comb of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*
-*Return:(0) - success
-*
-*/
+ *main - entry point
+ *
+ *Return:(0) - success
+ *
+ */
 int main(void)
 {
-int a, b, c;
+	int i, j, z;
 
-for(a = 0; a <=9; a++)
-{
-for (
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (z = 0; z <= 9; z++)
+			{
+				if (i != j && j != z && i != z && i < j && z > j)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(z + '0');
+					if (!(i == 7 && j == 8 && z == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
