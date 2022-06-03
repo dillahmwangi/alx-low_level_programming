b74868401082d4202b2dcedafdcd34fc51baeb5b
dillahmwangi <mwangimuthony@gmail.com>
2022-06-04 00:31:22 +0300

    print all base 16 in lower

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,23 @@
-#include
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description:prints numbers of base16 in lowercase
+ *Use putchar onlt 3 times
+ *Return: Always 0
+ */
+int main(void)
+{
+	int digit, letter, b;
+
+	for (digit = 48, b = 0; b < 10; digit++, b++)
+	{
+		putchar(digit);
+	}
+	for (letter = 97, b = 1; b < 7; letter++, b++)
+	{
+		putchar(letter);
+	}
+	putchar(10);
+	return (0);
+}
