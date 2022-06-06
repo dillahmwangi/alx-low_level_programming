d045a5ee3b7218e2d29db70f8aa7bd7acb691392
dillahmwangi <mwangimuthony@gmail.com>
2022-06-06 14:36:39 +0300

    print lowercase alphabets in reverse

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,22 +1,19 @@
-<<<<<<< HEAD
-=======
 #include <stdio.h>
 
 /**
- * main - print all single digit numbers of base 10 starting from
+ * main - program that prints all single digit numbers of base 10 starting from
  * All your code should be in the main function
  * Return: 0
  */
 
 int main(void)
 {
-int num;
-while (num < 10)
+int n;
+while (n < 10)
 {
-printf("%d", num);
-num += 1;
+printf("%d", n);
+n += 1;
 }
 printf("\n");
 return (0);
 }
->>>>>>> a3cacc76067257a5f1332c73d6dd5530b171b14a
