1d0a6638fdf9facdbce5021bd35234e554994edb
dillahmwangi <mwangimuthony@gmail.com>
2022-09-07 21:49:04 +0300

    header file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,7 +3,7 @@
 
 /*
  * File: 3-calc.h
- * Auth: Brennan D Baraban
+ * Auth: Charity Mwangi
  * Desc: Header file containing all structures and
  *       prototypes used by the 3-main.c program.
  */
